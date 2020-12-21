5038f431ad7c2ae92cbcc6cdff20a0deeebc5bb3
kojin higa <kojin.higa@gmail.com>
2020-12-21 20:46:20 +0900

    エラ〜メッセージを日本語化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
   def update
     @post.update!(post_params)
-    redirect_to post, notice: "更新しました"
+    redirect_to @post, notice: "更新しました"
   end
 
   private
